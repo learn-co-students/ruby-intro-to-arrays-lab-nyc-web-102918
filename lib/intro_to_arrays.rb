5a5e22d987c7d604d0a36a9738509de2cafe2aaa
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-11 18:44:29 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  []
+end 
+
+def array_with_two_elements
+  array=["Germain","Duran"]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2] 
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  first=array.shift
+end
+
+def last_element_with_array_methods(array)
+ last=array.pop
+end
+
+def length_of_array(array)
+  length=array.length
+end
